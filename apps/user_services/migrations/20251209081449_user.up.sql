04d20acd626e1f4d627dfc2579fcdaf17fed9b1a
Reihan1305 <freihan570@gmail.com>
2025-12-09 15:24:50 +0700

    feat: add user migrations

--- apps/user_services/migrations/20251209081449_user.up.sql
+++ apps/user_services/migrations/20251209081449_user.up.sql
@@ -1,1 +1,11 @@
 -- Add up migration script here
+CREATE TABLE IF NOT EXISTS Users (
+    id UUID PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    phone_number TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
